8d2b9204777e12e6dedc50f7dbfc38f92cb5960d
LiliiaBarsuk <lilya.b@live.com>
2023-01-31 14:28:54 +0200

    Update uniqueSymbolFinder.js

--- src/utils/uniqueSymbolFinder.js
+++ src/utils/uniqueSymbolFinder.js
@@ -1,35 +1,33 @@
+export function findUniqueSymbol (value) {
 
-
-export const findUniqueSymbol = (value) => {
     const arrayOfWords = value.replaceAll(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\n]/g, ' ').split(' ').filter(item => item !== '');
-    
+
     const uniqueSymbolsInWords = [];
 
     arrayOfWords.forEach(item => {
         const uniqueSymbolsArr = createUniqueSymbolsArray(item.split(''))
-
+        
         if(uniqueSymbolsArr.length > 0) {
-            uniqueSymbolsInWords.push(uniqueSymbolsArr[0])
+            uniqueSymbolsInWords.push(uniqueSymbolsArr[0])        
         }
     })
-
+    
     const uniqueSymbols = createUniqueSymbolsArray(uniqueSymbolsInWords);
-
+ 
     return uniqueSymbols[0]
 }
 
-const createUniqueSymbolsArray = (array) => {
+function createUniqueSymbolsArray (array) {
     const set = new Set() 
     
-    const uniqueElementsInArray = array.reduce((prevValue, item) => {
-        if (!prevValue.includes(item)) {
+    const uniqueElementsInArray = array.reduce((prevValue, item) => { 
+        if (!prevValue.includes(item)) {                               
             return [...prevValue, item]
-        } else {
+        } else {                                                         
             set.add(item)
            return prevValue 
         } 
-    }, []).filter(item => !set.has(item))
+    }, []).filter(item => !set.has(item))    
 
-   return uniqueElementsInArray
-
+   return uniqueElementsInArray   
 }
